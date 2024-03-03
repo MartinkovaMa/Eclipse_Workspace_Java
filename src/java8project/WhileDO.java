32f8a603caf50db3ec457065e74d8c7e47cce9d4
Martina Martínková <Martina.siblova@seznam.cz>
2024-03-03 10:31:42 +0100

    DOWHILE loops

---
+++ src/java8project/WhileDO.java
@@ -0,0 +1,18 @@
+package java8project;
+
+public class WhileDO {
+
+	public static void main(String[] args) {
+	int i = 1;
+	while(i<=100) {
+		System.out.println(i);
+		i++;
+	}
+//for,while,do - while - 3 possibilities
+	do {
+		System.out.println(i);
+	i++;
+	}while (i <=100);
+	}
+
+}
