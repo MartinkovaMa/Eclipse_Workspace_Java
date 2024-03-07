a39517c555bdc079564e9c92d72b36c82628370c
Martina Martínková <Martina.siblova@seznam.cz>
2024-03-07 15:57:16 +0100

    new tasks added

---
+++ src/codersLab/Main14.java
@@ -0,0 +1,24 @@
+package codersLab;
+
+public class Main14 {
+
+	public static void main(String[] args) {
+		for(int n=0;n<=6;n++) {
+			if (n % 2 == 0) {
+				System.out.println( n + " - even ");
+			} else {
+				System.out.println( n + " - odd ");
+			}
+		}
+		int m = 0;
+		while (m <= 6){
+			if (m % 2 == 0) {
+				System.out.println( m + " - even ");
+			} else {
+				System.out.println( m + " - odd");
+			}
+			m = m+1;
+	}
+	}
+
+}
