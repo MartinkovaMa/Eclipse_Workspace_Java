60957f04dad9b150638415bf16d7fe71a2559824
Martina Martínková <Martina.siblova@seznam.cz>
2024-03-08 14:06:54 +0100

    new tasks added

---
+++ src/codersLab/Main20.java
@@ -0,0 +1,11 @@
+package codersLab;
+
+public class Main20 {
+
+	public static void main(String[] args) {
+		int[]threeElements= new int []{3,4,5};
+		System.out.print(threeElements[0]+ " " + threeElements[1]+ " " + threeElements[2]+ " ");
+
+	}
+
+}
